8dfea4e75192a316fe849ebd270b4efe9f580260
Patryk Rubaszny <prubaszn@ilslogistics.eu>
2025-04-17 14:12:33 +0200

    Disable DataSource and JPA autoconfig

--- src/main/java/com/example/danxils_api/DanxilsApiApplication.java
+++ src/main/java/com/example/danxils_api/DanxilsApiApplication.java
@@ -2,11 +2,18 @@ package com.example.danxils_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(
+        exclude = {
+                DataSourceAutoConfiguration.class,
+                HibernateJpaAutoConfiguration.class
+        }
+)
 public class DanxilsApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(DanxilsApiApplication.class, args);
     }
 }
-
